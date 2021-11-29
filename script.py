9d6f1f52f08302e71b50a9b47761beee2b1f650c
ak3052005 <37368724+ak3052005@users.noreply.github.com>
2021-11-29 15:47:49 +0530

    Update script.py

--- script.py
+++ script.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
-# @trojanzhex
+# @pro_morningstar
 
 
 class Script(object):
@@ -13,7 +13,7 @@ Send me any video file and extract what you want :).
 
 See <i>help</i> for more details.
 
-© @TroJanzHEX</b>"""
+© @Movies_Unloaded</b>"""
 
 
     HELP_MSG = """<b>Hai, Follow these Steps..</b>
@@ -26,12 +26,12 @@ See <i>help</i> for more details.
 
 🌀 <i>Now select the stream you need to extract!</i>
 
-<b>© @TroJanzHEX</b>"""
+<b>© @Movies_Unloaded</b>"""
 
 
     ABOUT_MSG = """⭕️<b>My Name : Streams Extractor Bot</b>
 
-⭕️<b>Creater :</b> @TroJanzHEX    
+⭕️<b>Creater :</b> @pro_morningstar  
 
 ⭕️<b>Language :</b> <code>Python3</code>
 
